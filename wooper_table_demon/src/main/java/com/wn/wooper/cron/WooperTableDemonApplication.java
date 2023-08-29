86ebc257c75dd346444afc44d5da912bc4ce9e18
kimminsoo <kimminsoo@localhost>
2023-08-29 14:57:38 +0900

    우퍼 에이전트 결과 테이블 생성 데몬 초기 등록

---
+++ wooper_table_demon/src/main/java/com/wn/wooper/cron/WooperTableDemonApplication.java
@@ -0,0 +1,13 @@
+package com.wn.wooper.cron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooperTableDemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooperTableDemonApplication.class, args);
+	}
+
+}
---
+++ wooper_table_demon/src/test/java/com/wn/wooper/cron/WooperTableDemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wn.wooper.cron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooperTableDemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
